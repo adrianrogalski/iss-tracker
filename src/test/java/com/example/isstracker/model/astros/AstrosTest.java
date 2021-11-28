4679ff4328e8eab258aeb1142b50b7218a58c7f7
adrianrogalski <adrianrogalski99@gmail.com>
2021-11-28 21:27:05 +0100

    test: tested if Astros entity saves properly into the database

---
+++ src/test/java/com/example/isstracker/model/astros/AstrosTest.java
@@ -0,0 +1,95 @@
+package com.example.isstracker.model.astros;
+
+import com.example.isstracker.model.issnow.IssNow;
+import com.example.isstracker.model.issnow.IssPosition;
+import com.example.isstracker.repository.ApiRepository;
+import com.example.isstracker.repository.SimpleGenericRepository;
+import com.example.isstracker.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.IOException;
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+class AstrosTest {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private final static URI uri = URI.create("http://api.open-notify.org/astros.json");
+    private final SimpleGenericRepository<Astros> repository = new ApiRepository(Astros.class);
+    private Session session;
+    private Transaction transaction;
+
+    @BeforeEach
+    void before() {
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+    }
+
+    @AfterEach
+    void after() {
+        transaction.rollback();
+        session.close();
+    }
+
+    @Test
+    void shouldSaveAstrosIntoDatabase() throws IOException, InterruptedException {
+        // given
+        Astros astros = repository.findByURI(uri).get();
+
+        // when
+        saveAndFlush(astros);
+        Astros databaseRead = session.get(Astros.class, astros.getId());
+
+        // then
+        System.out.println(astros);
+        System.out.println(databaseRead);
+        System.out.println(astros.equals(databaseRead));
+        assertEquals(astros, databaseRead);
+    }
+
+    void shouldHaveSuccessValueInTableMessageField() throws IOException, InterruptedException {
+        // given
+        Astros astros = repository.findByURI(uri).get();
+
+        // when
+        saveAndFlush(astros);
+        Astros databaseRead = session.get(Astros.class, astros.getId());
+
+        String message = databaseRead.getMessage();
+        // then
+        assertEquals("success", message);
+    }
+
+    @Test
+    void peopleListShouldntBeEmpty() throws IOException, InterruptedException {
+        // given
+        Astros astros = repository.findByURI(uri).get();
+
+        // when
+        saveAndFlush(astros);
+        Astros databaseRead = session.get(Astros.class, astros.getId());
+        assertFalse(databaseRead.getPeople().isEmpty());
+    }
+
+    private void saveAndFlush(Astros astros) {
+        session.save(astros);
+        session.flush();
+        session.clear();
+    }
+
+
+
+
+
+}
