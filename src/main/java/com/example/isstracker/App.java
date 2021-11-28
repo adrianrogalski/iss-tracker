0cd9b7924a607184eb359820871b52bee6f95ffa
adrianrogalski <adrianrogalski99@gmail.com>
2021-11-28 14:30:59 +0100

    test: added tests for ApiRepository class

--- src/main/java/com/example/isstracker/App.java
+++ src/main/java/com/example/isstracker/App.java
@@ -10,13 +10,6 @@ import java.util.Optional;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
-        final URI ISS_URI = URI.create("http://api.open-notify.org/iss-now.json");
-        final URI ASTROS_URI = URI.create("http://api.open-notify.org/astros.json");
-        ApiRepository<IssNow> issNowRepo = new ApiRepository(IssNow.class);
-        ApiRepository<Astros> astrosRepo = new ApiRepository(Astros.class);
-        Optional<IssNow> iss = issNowRepo.get(ISS_URI);
-        Optional<Astros> astros = astrosRepo.get(ASTROS_URI);
-        System.out.println(iss.get());
-        System.out.println(astros.get());
+
     }
 }
---
+++ src/test/java/com/example/isstracker/repository/ApiRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.example.isstracker.repository;
+
+import com.example.isstracker.model.astros.Astros;
+import com.example.isstracker.model.issnow.IssNow;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiRepositoryTest {
+
+    @Test
+    void findByURIShouldReturnOptionalWithIssObjectGivenValidIssNowURI() throws IOException, InterruptedException {
+        // given
+        final URI ISS_URI = URI.create("http://api.open-notify.org/iss-now.json");
+        ApiRepository<IssNow> issNowRepo = new ApiRepository(IssNow.class);
+        // when
+        Optional<IssNow> iss = issNowRepo.findByURI(ISS_URI);
+        // then
+        Assertions.assertTrue(iss.isPresent());
+    }
+
+    @Test
+    void findByURIShouldReturnOptionalWithAnAstrosObjectGivenValidAstrosURI() throws IOException, InterruptedException {
+        // given
+        final URI ASTROS_URI = URI.create("http://api.open-notify.org/astros.json");
+        ApiRepository<Astros> astrosRepo = new ApiRepository(Astros.class);
+        // when
+        Optional<Astros> iss = astrosRepo.findByURI(ASTROS_URI);
+        // then
+        Assertions.assertTrue(iss.isPresent());
+    }
+
+    @Test
+    void findByURIShouldReturnEmptyOptionalGivenWrongURI() throws IOException, InterruptedException {
+        // given
+        final URI ISS_URI = URI.create("http://api.open-notify.org/notindatabase.json");
+        ApiRepository<IssNow> issNowRepo = new ApiRepository(IssNow.class);
+        // when
+        Optional<IssNow> iss = issNowRepo.findByURI(ISS_URI);
+        // then
+        Assertions.assertTrue(iss.isEmpty());
+    }
+}
