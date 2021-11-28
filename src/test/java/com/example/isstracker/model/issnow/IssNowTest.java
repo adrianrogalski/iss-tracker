81616e2193e131486273aa63063e29aa4e10c13b
adrianrogalski <adrianrogalski99@gmail.com>
2021-11-28 18:30:48 +0100

    test: tested functionality of IssNow entity, checked if it saves properly into the database

---
+++ src/test/java/com/example/isstracker/model/issnow/IssNowTest.java
@@ -0,0 +1,77 @@
+package com.example.isstracker.model.issnow;
+
+import com.example.isstracker.repository.ApiRepository;
+import com.example.isstracker.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IssNowTest {
+    private final SessionFactory factory = HibernateUtil.getSessionFactory();
+    final URI ISS_URI = URI.create("http://api.open-notify.org/iss-now.json");
+    ApiRepository<IssNow> issNowRepo = new ApiRepository(IssNow.class);
+    private Session session;
+    private Transaction transaction;
+
+    @BeforeEach
+    void before(){
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+    }
+
+    @AfterEach
+    void after() {
+        transaction.rollback();
+        session.close();
+    }
+
+    @Test
+    void shouldSaveIssNowIntoDatabase() throws IOException, InterruptedException {
+        // given
+        IssNow iss = issNowRepo.findByURI(ISS_URI).get();
+        // when
+        saveAndFlush(iss);
+        final IssNow readIssNow = session.get(IssNow.class, iss.getId());
+        // then
+        assertEquals(iss, readIssNow);
+    }
+
+    @Test
+    void shouldHaveSuccessValueInTableMessageField() throws IOException, InterruptedException {
+        // given
+        IssNow iss = issNowRepo.findByURI(ISS_URI).get();
+        // when
+        saveAndFlush(iss);
+        final IssNow readIssNow = session.get(IssNow.class, iss.getId());
+        String message = readIssNow.getMessage();
+        // then
+        assertEquals("success", message);
+    }
+
+    @Test
+    void issNowPositionFieldShouldHaveEmptyLatitudeAndLongitude() throws IOException, InterruptedException {
+        // given
+        IssNow iss = issNowRepo.findByURI(ISS_URI).get();
+        // when
+        saveAndFlush(iss);
+        final IssNow readIssNow = session.get(IssNow.class, iss.getId());
+        IssPosition position = readIssNow.getIss_position();
+        assertFalse(position.getLatitude().isEmpty());
+        assertFalse(position.getLongitude().isEmpty());
+    }
+
+    private void saveAndFlush(IssNow iss) {
+        session.save(iss);
+        session.flush();
+        session.clear();
+    }
+
+}
